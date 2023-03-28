de6cb933fa1db1813ff9634968e34ea76b32fa40
Maciek332 <feliks5100@gmail.com>
2023-03-28 23:04:35 +0200

    komunikaty przy rejestracji

--- ChatApp/ChatApp/Views/LoginFormPage.xaml.cs
+++ ChatApp/ChatApp/Views/LoginFormPage.xaml.cs
@@ -1,6 +1,20 @@
 ﻿using ChatApp.ViewModels;
-
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Controls;
+using CommunityToolkit.Common;
 
 namespace ChatApp.Views;
 
@@ -15,5 +29,39 @@ public sealed partial class LoginFormPage : Page
     {
         ViewModel = App.GetService<LoginFormViewModel>();
         InitializeComponent();
+    }
+
+    private void RegisterClick(object sender, RoutedEventArgs e)
+    {
+        string RegisterResult;
+        if ( this.RegEmail.Text !=""  || this.RegPassword.Password != "" || this.RegUsername.Text != "")
+        {
+            if (this.RegEmail.Text.IsEmail())
+            {
+                RegisterResult = "Pomyślnie zarejestrowano z danymi:" + "\nE-mail: " + this.RegEmail.Text + "\n Login: " + this.RegUsername.Text + "\n Hasło: " + this.RegPassword.Password;
+                this.RegisterSuccess.Message = RegisterResult;
+                this.RegisterFali.IsOpen = false;
+                this.RegisterSuccess.IsOpen = true;
+
+
+            }
+            else
+            {
+                RegisterResult = "Wprowadź poprawy E-mail";
+                this.RegisterFali.Message = RegisterResult;
+                this.RegisterSuccess.IsOpen = false;
+                this.RegisterFali.IsOpen = true;
+            }
+            
+        }
+        else
+        {
+            RegisterResult = "Pola nie mogą być puste";
+            this.RegisterFali.Message = RegisterResult;
+            this.RegisterSuccess.IsOpen = false;
+            this.RegisterFali.IsOpen = true;
+        }
+
+
     }
 }

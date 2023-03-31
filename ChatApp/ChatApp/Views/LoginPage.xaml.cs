bc012ee9ace6e6d3272983307d544c89dc18da76
Ania W <100371061+AnaShiro@users.noreply.github.com>
2023-03-31 20:08:05 +0200

    strona logowania

--- ChatApp/ChatApp/Views/LoginPage.xaml.cs
+++ ChatApp/ChatApp/Views/LoginPage.xaml.cs
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
-
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -16,19 +13,16 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace ChatApp.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class LoginPage : Page
     {
         public LoginPage()
         {
             this.InitializeComponent();
         }
+
+
     }
 }
